1d63f7a201a067dc19a45e8ab385ebc916fca5bd
Kit <dev_kit@icloud.com>
2015-03-05 15:49:14 -0700

    add

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'home' => 'home#index'
 
-  get "/auth/twitter/callback" => "sessions#create"
-  get "/signout" => "sessions#destroy", :as => :signout
+  get "/auth/twitter/callback" => "session#create"
+  get "/signout" => "session#destroy", :as => :signout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
