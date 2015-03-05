d0d4d1f28f5027c3345dd7ebffa674ba8fda45fd
Kit <dev_kit@icloud.com>
2015-03-05 15:32:41 -0700

    add

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,12 @@
 class SessionController < ApplicationController
   def create
-    auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
-    session[:user_id] = user.id
-    redirect_to home_path, :notice => "Signed in!"
+    @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to home_path, :notice => "Signed in!"
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
